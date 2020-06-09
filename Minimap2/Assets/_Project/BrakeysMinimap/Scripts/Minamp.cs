a264a5aa8b85be23888fdfd480423a9eb448933d
Vazquez, Jose J <jose.vazquez2@thermofisher.com>
2020-06-08 19:28:54 -0700

    Add minimap Brakeys aproach using ortographic cam to render the miniamp view into a texture

---
+++ Minimap2/Assets/_Project/BrakeysMinimap/Scripts/Minamp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    /// <summary>
+    /// Put this script in the Minimap Camera
+    /// </summary>
+    public class Minamp : MonoBehaviour
+    {
+        [SerializeField] private Transform _player;
+        [SerializeField] private bool fixedNorth;
+
+
+        private void LateUpdate()
+        {
+            FollowPlayer(_player);
+            RotateMinimap(_player);
+        }
+
+        private void RotateMinimap(Transform player)
+        {
+            transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        }
+
+        private void FollowPlayer(Transform player)
+        {
+            Vector3 newPosition = player.position;
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+        }
+    }
+}
